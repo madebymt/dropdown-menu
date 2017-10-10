7605c79a5d3923166556fe81f9be3d9daa09dea4
Ruby <c19890611@gmail.com>
2017-10-10 15:48:56 -0500

    process bar

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-
 $(function() {
-       $(".project").on('click', function() {
-           $(this).parent().find('.details').slideDown('slow');
-           return false;
-       });
-   });
-//
+  $(".project").on('click', function() {
+    $(this).parent().find('.details').slideDown('slow');
+    return false;
+  });
+});
+
+function increase() {
+  let elem = document.querySelector("#myBar")
+  let width = 10
+  let id = setInterval(frame, 10)
+
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id)
+    } else {
+      width++
+      elem.style.width = width + '%';
+      elem.innerHTML = width * 1 + '%'
+
+    }
+  }
+}
--- app2.js
+++ app2.js
@@ -1,7 +1,41 @@
 // slidetoggle make it click dropdown and close back
 
 $(function() {
-       $(".project").on('click', function() {
-           $(this).parent().find('.details').slideToggle();
-       });
-   });
+  $(".project").on('click', function() {
+    $(this).parent().find('.details').slideToggle();
+  });
+});
+
+
+function increase() {
+  let elem = document.querySelector("#myBar")
+  let width = 10
+  let id = setInterval(frame, 10)
+
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id)
+    } else {
+      width++
+      elem.style.width = width + '%';
+      elem.innerHTML = width * 1 + '%'
+
+    }
+  }
+}
+
+
+function move() {
+  var elem = document.getElementById("myBar");
+  var width = 1;
+  var id = setInterval(frame, 10);
+
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++;
+      elem.style.width = width + '%';
+    }
+  }
+}
